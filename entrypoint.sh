650c3d8bccc996d2808b71637d40ba0864b5c2de
mihir <mihirsin@usc.edu>
2025-08-05 16:29:15 -0700

    added launch file for subscriber only, added roslaunch to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 source /opt/ros/noetic/setup.bash
 [ -f /opt/barracuda-thruster-output-controller/catkin_ws/devel/setup.bash ] && source /opt/barracuda-thruster-output-controller/catkin_ws/devel/setup.bash
-exec tail -f /dev/null
+# exec tail -f /dev/null
+roslaunch barracuda_thruster_output_controller thruster_controller.launch
