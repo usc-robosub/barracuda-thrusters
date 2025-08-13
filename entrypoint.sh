9c9a4c0aaee68b93e9a26de80ab958aadf80820a
Russell Rodriguez <rar72397@usc.edu>
2025-08-13 12:18:51 -0700

    set up clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ source /opt/barracuda-thruster-output-controller/catkin_ws/devel/setup.bash
 # add args to this entrypoint to do different execs (I think CMD gets overrided)
 # exec tail -f /dev/null
 # roslaunch --wait barracuda_thruster_output_controller thruster_controller.launch
-roslaunch barracuda_thruster_output_controller thruster_controller.launch
+roslaunch barracuda_thruster_output_controller thruster_controller.launch --wait
