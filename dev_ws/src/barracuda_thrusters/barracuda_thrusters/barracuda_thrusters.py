699dd5bce659a380471a76bb4b5f3002dddd84b5
mihir <mihirsin@usc.edu>
2025-10-27 13:45:59 -0700

    remove verify configs function

--- dev_ws/src/barracuda_thrusters/barracuda_thrusters/barracuda_thrusters.py
+++ dev_ws/src/barracuda_thrusters/barracuda_thrusters/barracuda_thrusters.py
@@ -14,7 +14,6 @@ class BarracudaThrusters(Node):
         # self.declare_parameter('n_thrusters', 8)
         # self.n_thrusters = self.get_parameter('n_thrusters').value
         self.n_thrusters = 8    
-        self.verify_config_values()
 
         for thruster_idx in range(self.n_thrusters):
             topic = f'thrusters/input{thruster_idx}'
@@ -25,26 +24,6 @@ class BarracudaThrusters(Node):
                 10
             )
 
-    def verify_config_values(self):
-        if teensy.GPIO is None:
-            self.get_logger().info('teensy.GPIO is None: no I2C available')
-            return
-        for i2c_addr in teensy.i2c_addresses:
-            teensy_pwm_freq = teensy.read_i2c_16(i2c_addr, teensy.PWM_FREQ_REG)
-            teensy_pwm_bit_res = teensy.read_i2c_16(i2c_addr, teensy.PWM_BIT_RES_REG)
-            teensy_t200_init = teensy.read_i2c_16(i2c_addr, teensy.T200_INIT_REG)
-
-            if teensy_pwm_freq is None or teensy_pwm_bit_res is None or teensy_t200_init is None:
-                self.get_logger().error(f'could not read from {i2c_addr:#04x}')
-                continue
-
-            if teensy_pwm_freq != f2pwm.PWM_FREQ:
-                self.get_logger().warn(f'PWM freq at {i2c_addr:#04x} not equal to f2pwm.PWM_FREQ')
-            if teensy_pwm_bit_res != f2pwm.PWM_BIT_RES:
-                self.get_logger().warn(f'PWM bit res at {i2c_addr:#04x} not equal to f2pwm.PWM_BIT_RES')                
-            if teensy_t200_init != f2pwm.T200_INIT:
-                self.get_logger().warn(f'PWM T200 init at {i2c_addr:#04x} not equal to f2pwm.T200_INIT')
-                
     def subscriber_callback(self, msg, thruster_idx):
         thruster_force_newtons = msg.data
         pwm_duty_cycle_val = f2pwm.to_duty_cycle(thruster_force_newtons)
