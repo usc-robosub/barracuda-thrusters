ae56583cb6d1c66c7460caf51ef61e19a1f0690d
mihir <mihirsin@usc.edu>
2025-10-28 17:36:52 -0700

    changes to test thrusters service

--- dev_ws/src/barracuda_thrusters/barracuda_thrusters/test_thrusters_service.py
+++ dev_ws/src/barracuda_thrusters/barracuda_thrusters/test_thrusters_service.py
@@ -3,16 +3,25 @@ from rclpy.node import Node
 from std_msgs.msg import Float32
 from barracuda_thrusters_interfaces.srv import TestThrusters
 
+NTHRUSTERS = 8
+
 class TestThrustersService(Node):
     def __init__(self):
         super().__init__('test_thrusters_service')
         self.srv = self.create_service(TestThrusters, 'test_thrusters_service', self.test_thrusters_callback)
         self.get_logger().info('Created service')
+        self.pubs = [None] * NTHRUSTERS
+        for thruster_idx in range(NTHRUSTERS):
+            self.pubs[thruster_idx] = self.create_publisher(Float32, f'thrusters/input{thruster_idx}', 10)
 
     def test_thrusters_callback(self, request, response):
-        if request.thrusters == []:
-            request.thrusters = [0, 1, 2, 3, 4, 5, 6, 7]
-        self.get_logger().info('Incoming request\na: %d b: %d' % (request.thrusters, request.force))
+        response.res = f'Publishing force {request.force} to thruster topics: '
+        thrusters = [i for i in range(NTHRUSTERS)] if not request.thrusters else request.thrusters
+        for thruster_idx in thrusters:
+            msg = Float32()
+            msg.data = request.force
+            self.pubs[thruster_idx].publish(msg)
+            response.res += f'{thruster_idx} '
         return response
 
 def main():
